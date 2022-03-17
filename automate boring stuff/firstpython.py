7fec07fe58da3dfeff5e943f075303d24db8737e
Tildes <tildesare.cool@gmail.com>
2022-03-16 19:13:17 -0700

    starting automate playlist

---
+++ automate boring stuff/firstpython.py
@@ -0,0 +1,17 @@
+# this program says hell and ask for my name
+
+print('Hello World')
+
+
+
+print('What is your name?') # ask for user input
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+
+
+
+print('What is your age? ') # get user age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
