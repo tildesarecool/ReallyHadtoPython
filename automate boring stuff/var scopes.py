2bc963905bbdc3ce085458314636e75e2479c1ad
Tildes <tildesare.cool@gmail.com>
2022-03-17 19:18:26 -0700

    on to video 11

--- automate boring stuff/var scopes.py
+++ automate boring stuff/var scopes.py
@@ -1,7 +1,23 @@
 # global variable
 spam = 42
-def eggs:
+def eggs():
     # local vaariable
     spam = 42
     # not supposed to onflict since different scopes
 
+def spam():
+#    eggs = 99
+ #   bacon()
+    eggs = 'Hello'
+    print(eggs)
+
+def bacon():
+    ham = 101
+    eggs = 0
+
+
+eggs = 42
+
+spam()
+
+print(eggs)
