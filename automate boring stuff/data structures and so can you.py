9224fd7109e215b7d549148ef84d621ce768317b
tildesarecool <tildesare.cool@gmail.com>
2022-03-19 12:59:18 -0700

    minor update

---
+++ automate boring stuff/data structures and so can you.py
@@ -0,0 +1,15 @@
+# lesson 18: data structures (udemy)
+# comes in at a merciful 11 minutes
+# pages 112 - 117 or so
+
+# lists/dictionaries can be used for structured data
+
+cat = {'name': 'Zophie', 'age': 7, 'color': 'gray'} # create dictionary
+allCats = [] # blank list
+allCats.append( {'name': 'Zophie', 'age': 7, 'color': 'gray'}  )
+allCats.append( {'name': 'Pooka', 'age': 5, 'color': 'black'}  )
+allCats.append( {'name': 'fat-tail', 'age': 5, 'color': 'black'}  )
+allCats.append( {'name': '???', 'age': 0, 'color': 'orange'}  )
+
+# list of dictionaries = data structures
+
