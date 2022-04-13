ead06309dd48b9e0c119ca555808d98b07a0a47e
tildesarecool <tildesare.cool@gmail.com>
2022-04-12 23:45:45 -0700

    minor changes

--- SteamScraper/jsonexperiment.py
+++ SteamScraper/jsonexperiment.py
@@ -1,6 +1,6 @@
 import json
 
-json1 = '''{"platform":"Nokia", "ip":"1.1.1.1"}'''
+json1 = '''{"platform":"Nokia", "ip":"1.1.1.1"},{"platform":"Nokia", "ip":"1.1.1.1"}'''
 
 dict1 = json.loads(json1)
 
@@ -20,12 +20,17 @@ json3 = json.loads(json2)
 #print(json3)
 #print(json3.get('ip'))
 
-GameDict = '''{
-"appid":1025480,"name":"1-Bit Revival: The Residuals of Null","app_type":1,"logo":"https://cdn.akamai.steamstatic.com/steam/apps/1025480/capsule_184x69.jpg","friendlyURL":1025480,"availStatLinks":{"achievements":true,"global_achievements":true,"stats":false,"gcpd":false,"leaderboards":false,"global_leaderboards":false},"hours_forever":"0.6","last_played":1642216987
-}'''
+GameDict = '{"appid":1025480,"name":"1-Bit Revival: The Residuals of Null","app_type":1,"logo":"https://cdn.akamai.steamstatic.com/steam/apps/1025480/capsule_184x69.jpg","friendlyURL":1025480,"availStatLinks":{"achievements":true,"global_achievements":true,"stats":false,"gcpd":false,"leaderboards":false,"global_leaderboards":false},"hours_forever":"0.6","last_played":1642216987},{"appid":1025480,"name":"1-Bit Revival: The Residuals of Null","app_type":1,"logo":"https://cdn.akamai.steamstatic.com/steam/apps/1025480/capsule_184x69.jpg","friendlyURL":1025480,"availStatLinks":{"achievements":true,"global_achievements":true,"stats":false,"gcpd":false,"leaderboards":false,"global_leaderboards":false},"hours_forever":"0.6","last_played":1642216987}'
 
 print(type(GameDict))
 jsonGame1 = json.loads(GameDict)
 print(type(jsonGame1))
 
 print(jsonGame1.get("app_type"))
+
+json4 = { }
+
+json4.values()
+
+print('the values are: ')
+print(jsonGame1.values())
