569e7a7da535d4b5fd996dcc54c83cdc44daf78b
tildesarecool <tildesare.cool@gmail.com>
2023-07-11 17:40:28 -0700

    starting a new course

---
+++ complete guide to Python (clearcode)/firstpython.py
@@ -0,0 +1,14 @@
+# this is 11 hour youtube video
+# https://www.youtube.com/watch?v=mDKM-JtUhhc
+# I found out this part was on youtube
+# and the next part "lean python by making games"
+# is on udemy
+# https://www.udemy.com/course/learn-python-by-making-games/
+# so it thought I'd go through the youtube part before buying it
+
+# websites that can run python (and others) online:
+# programiz.com...i don't actually see the interactive shell demonstrated in the video but i don't need it anyway
+# also replit.com
+
+
+
