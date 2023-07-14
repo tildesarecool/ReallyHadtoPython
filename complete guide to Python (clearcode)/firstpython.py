9795515cf3d88f0aa1e8cb7b66e1ec9f91d700db
tildesarecool <tildesare.cool@gmail.com>
2023-07-13 18:56:44 -0700

    solved the slicing excer albeit from brute force

--- complete guide to Python (clearcode)/firstpython.py
+++ complete guide to Python (clearcode)/firstpython.py
@@ -10,7 +10,7 @@
 # programiz.com...i don't actually see the interactive shell demonstrated in the video but i don't need it anyway
 # also replit.com
 
-print( 7 // 2 ) # not sure i knew about this truncating decimal (not rounding) -  output is just 3
+#print( 7 // 2 ) # not sure i knew about this truncating decimal (not rounding) -  output is just 3
 
 
 # function - 57 min or so in
@@ -23,3 +23,107 @@ print( 7 // 2 ) # not sure i knew about this truncating decimal (not rounding) -
 # hopefully i didn't skip too much intersting things
 # a lot like list: tuples - immutable 
 
+# tuples/lists
+# both can contain any kind of data, including lists and tuples and bools etc - used with parans ( )
+
+#list is similar - created with [ ] and separate values with commas
+
+# tuples are immutable
+
+# can append lists with list.apopend(value)
+# to do same with tuples can create a new tuple with old values and some additional
+# technically tuples slightly faster
+
+# start with lists
+
+
+#my_list = [1,2,3,4,5, 'word']
+#print(my_list)
+#print(len(my_list))
+
+# lists with methods
+#my_list.clear()
+#my_list.reverse()
+#print(my_list)
+
+#my_list.append(10)
+#print(my_list)
+
+
+# move on to tuples
+
+#my_tuple = (1,2,3,4,5,1.45, 'word', [7,8,9])
+
+#print(my_tuple)
+
+
+# how to pick elements from a list - how to get second element?
+
+# each value as an index number - starting from 0
+
+# so [1,2,3,4][1] = value of 2 as it's index 1 starting from 0
+
+#print(my_list[0])  # 0 refers to first index/index 0
+
+#print(my_tuple[7][0]) # my_tuple[7][0] - this returns first element in list inside the tuple, which the value 7
+
+# can also do the [-1] so it loops round to the list element
+
+
+
+#excercise_list = ['first entry', [123,456,[0,'Hello :)']], 'bye']
+#print(len(excercise_list))
+#print(excercise_list[1][2][1])
+
+# 2:27:07 - slicing
+
+# "how to pick multple elemetns form a list"
+# [1,2,3,4] - how to get the second and third element?
+
+
+# slicing involves [ ] like indexing, but you can 2 numbers separated by a colon first start/end number
+
+# in this lsit
+#test_list = [1,2,3,4,5,6,7,8,9,10]
+# could use [1:2] to refer to the 2 and 3
+# "python only goes to the end, does not include the end"
+
+
+
+# print(test_list[1:2]) # returns '[2]' literally
+
+# and [1:1]  would return '[]' literally
+
+# but [1:2:1] would indicate the direction - which defaults to 1 - which is positive
+
+
+
+
+#print(test_list[0:8]) # first 8 values in the list
+
+#print(test_list[0:8:2]) # output is [1, 3, 5]
+
+# using -1 in place of 2 would just be empty brackets of output, but....
+
+#print(test_list[-1:4:-1]) # output is [10, 9, 8, 7, 6]
+
+# can also leave value empty when using slicing, as in [1,2,3,4,5,6,7,8,9,10][::-1]
+
+#print(test_list[::-1]) # output [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] - just uses default values, which is first value in list; end value last value in list, defautl step size +1 increment
+
+# worktihng through with variables
+#default_slicing = test_list[::]
+#print(default_slicing)
+
+# excercise 
+# start from 8 and go to 2 values - pick every second value
+
+
+test_list = [1,2,3,4,5,6,7,8,9,10]
+
+excercise_slicing = test_list[::]
+
+
+solution_slice = excercise_slicing[-3:0:-2]
+
+print(solution_slice)
