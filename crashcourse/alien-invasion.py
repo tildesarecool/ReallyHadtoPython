5895f0c9444497a7b007fe6d41ca93e7132720a0
tildesarecool <tildesare.cool@gmail.com>
2024-01-19 16:24:17 -0800

    started on this book

---
+++ crashcourse/alien-invasion.py
@@ -0,0 +1,46 @@
+#  19 jan 2023
+#  alien invasion game from the book "python crash course"
+import sys, pygame
+from settings import Settings
+# import pygame
+
+class AlienInvasion: 
+    # Overall class to manage game assets and behavior 
+    def __init__(self):
+        # Initialize the game, and create game resources
+        pygame.init()
+        self.clock = pygame.time.Clock() # related to consistent frame rate - see also tick method
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (
+            self.settings.screen_width, 
+            self.settings.screen_height
+            )
+        )
+        
+        #self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+        # set background color
+        #self.bg_color = (230,230,230)
+        
+    def run_game(self):    
+        """start the main loop for the game"""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # redraw screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color) # 230,230,230 = light gray
+                    
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
+            self.clock.tick(60) # related to consistent framerate - see also the self.clock line in the init function
+                
+    if __name__ == '__main__':
+        
+        # Make a game instance, and run the game
+        ai = AlienInvasion()
+        ai.run_game()
+        
---
+++ crashcourse/settings.py
@@ -0,0 +1,9 @@
+# settings.py - settings for alien invasion script
+class Settings:
+    """a class to store all settings for alien invasion"""
+    def __init__(self):
+        """initialize game's settings."""
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
