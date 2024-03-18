3cff658a11181580f6f32c30238316a3dd72458f
tildesarecool <tildesare.cool@gmail.com>
2024-03-18 13:51:04 -0700

    i can move the box in all four directions AND am constricted to the boundaries of the window. so you know. monumental progress.

--- FroggerTangles/frog.py
+++ FroggerTangles/frog.py
@@ -7,7 +7,7 @@ import pygame as pyg
 
 from pygame.sprite import Sprite
 
-
+from common import SCREEN_WIDTH, SCREEN_HEIGHT
 #SCREEN_WIDTH = 800
 #SCREEN_HEIGHT = 600
 #GREEN: str = '#008000'
@@ -23,12 +23,15 @@ class Frog(GameRect, Sprite):
 
         self.xpos = xpos
         self.ypos = ypos
+        self.ypos_start = ypos
         self.width = width
         self.height = height
         self.color = color
         
         self.moving_left = False
         self.moving_right = False
+        self.moving_up = False
+        self.moving_down = False
         
 # gpt suggested these changes: the self.image lines here in __init__ and moving the self.rect line up here
 # i asked gpt if i could just use simple rectangle rather making it a surface so I'm going to try it that way first
@@ -52,9 +55,93 @@ class Frog(GameRect, Sprite):
             self.moving_left = True
         else:
             self.moving_left = False
+##################################################
             
+        keys = pyg.key.get_pressed()
+        if keys[pyg.K_d]:
+            self.moving_right = True
+            #print(f"screen width is {SCREEN_WIDTH}")
+        else:
+            self.moving_right = False
+##################################################
+
+        keys = pyg.key.get_pressed()
+        if keys[pyg.K_w]:
+            self.moving_up = True
+            #print(f"screen height is {SCREEN_HEIGHT}")
+        else:
+            self.moving_up = False
+            
+##################################################
+
+        keys = pyg.key.get_pressed()
+        if keys[pyg.K_s]:
+            
+            self.moving_down = True
+            print(f"screen height is {SCREEN_HEIGHT}")
+        else:
+            self.moving_down = False
+            
+##################################################
+            
+        if self.moving_right:
+            
+            if self.xpos <= float(SCREEN_WIDTH - self.width):
+                self.xpos += 3
+                #print(f"Current x position is {self.xpos}")
+                #print(f"Current rect right value is {self.rect.right}")
+            elif self.xpos >= float(SCREEN_WIDTH - self.width - 5):
+                #print(f"Value of SCREEN_WIDTH - self.rect.width is {float(SCREEN_WIDTH - self.width)}")
+                #print(f"Current x position is in else is {self.xpos}")
+                self.xpos = float(SCREEN_WIDTH - self.width - 5)
+##################################################
+
         if self.moving_left:
-            self.xpos -= 3
+            #self.xpos -= 3
+            if self.xpos > 0:
+                self.xpos -= 3
+                #print(f"Current x position is {self.xpos}")
+                #print(f"Current rect right value is {self.rect.right}")
+            elif self.xpos <= 5.0:
+                #print(f"Value of SCREEN_WIDTH - self.rect.width is {float(SCREEN_WIDTH - self.width)}")
+                #print(f"Current x position is in else is {self.xpos}")
+                self.xpos = 5.0
+
+##################################################
+
+        if self.moving_up:
+
+            #if self.ypos <= SCREEN_HEIGHT:
+            if self.ypos <= self.ypos_start:
+                self.ypos -= 3
+
+            elif self.rect.top <= 5.0:
+                self.ypos = 5.0
+
+##################################################
+
+        if self.moving_down:
+            print(f"Value of self.ypos_start is {self.ypos_start} ")
+            print(f"Value of self.ypos is {self.ypos} ")
+            #if self.ypos <= SCREEN_HEIGHT + self.height:#self.ypos_start:
+            if self.ypos <= self.ypos_start - 5:
+                self.ypos += 3
+            elif self.rect.bottom >= self.ypos_start:
+                print(f"Value of self.ypos_start is {self.ypos_start} ")
+                #print(f"Value of self.ypos is {self.ypos} ")
+                print(f"Current y position is in else is {self.ypos}")
+                print(f"self.rect.bottom value is {self.rect.bottom}")
+                #self.ypos = float(self.ypos_start - 5)
+                self.rect.bottom = float(self.ypos_start - 1)
+                #self.ypos = 5.0
+
+            #elif self.rect.bottom <= (SCREEN_HEIGHT + self.height + 50):
+                #self.ypos = self.ypos_start
+                #self.rect.bottom = self.ypos_start + 1
+                
+
+
+##################################################
 
 
 '''
