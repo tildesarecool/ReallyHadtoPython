4411f8da96e351d19446cb5b9ccfa926e0cf1f99
tildesarecool <tildesare.cool@gmail.com>
2024-05-10 11:50:15 -0700

    started working on ch 9 ex 24 in bootdev

--- bootdev/bootdev - ch. 09 c24 filter messages/main.py
+++ bootdev/bootdev - ch. 09 c24 filter messages/main.py
@@ -6,12 +6,16 @@ chap 9 #24
 FILTER MESSAGES
 You are about to write a bit more code in a single function than you have before.
 
-Do not try to write it all at once. Start with the outermost loop, and work your way inwards. Add extra print() statements and run your code often to make sure it's doing what you expect. Just make sure to remove the extra print() statements before submitting your code.
+Do not try to write it all at once. Start with the outermost loop, and work your way inwards. 
+Add extra print() statements and run your code often to make sure it's doing what you expect. 
+Just make sure to remove the extra print() statements before submitting your code.
 
-Running your code often to make sure each line is doing what you expect is called "debugging". All programmers, even seasoned professionals, break large problems down into small ones that they can debug line by line.
+Running your code often to make sure each line is doing what you expect is called "debugging". 
+All programmers, even seasoned professionals, break large problems down into small ones that they can debug line by line.
 
 ASSIGNMENT
-We need to filter the profanity out of our game's live chat feature! Complete the filter_messages function. It takes a list of chat messages as input and returns 2 new lists:
+We need to filter the profanity out of our game's live chat feature! Complete the filter_messages function. 
+It takes a list of chat messages as input and returns 2 new lists:
 
 A list of the same messages but with all instances of the word dang removed.
 A list containing the number of dang words that were removed from the message at that particular index.
@@ -53,8 +57,57 @@ print(sentence)
 
 '''
 
-
-
-
 def filter_messages(messages):
-    pass
+    #print(messages[0])
+    final_mess_str = " "
+    for i in range(len(messages)):
+        filt_message = str(messages[i])
+        print(f"filt message as string (in a for loop) is -- {filt_message} --")
+        split_mess = filt_message.split()
+
+        for j in range(len(split_mess)):
+            if split_mess[j] == "dang":
+                
+                split_mess[j] = "darn"
+                print(f"Found a dang - new entry is {split_mess}")
+            else:
+                print("No dang given")
+        rejoined = " "
+    
+        rejoined = rejoined.join(split_mess)
+        
+        print(f"rejoined split mess is -- {rejoined} --")
+        
+        final_mess_str = final_mess_str + rejoined + " "
+
+    print(f"rejoined filt message is -- {rejoined} -- (after for loop)")
+    print(f"final message is -- {final_mess_str} -- (after for loop)")
+#    rejoined = " "
+#
+#    rejoined = rejoined.join(split_mess)
+#    
+#    print(f"rejoined split mess is -- {rejoined} --")
+    
+
+
+    
+#    for i in range(len(messages)):
+#    for "dang" in filt_message:
+#        print("Found a dang")
+
+#    print(f"message as string is -- {filt_message} --")
+#    
+#    split_mess = "this is a string".split()
+#    print(f"split message as string is -- {split_mess} --")
+
+#    split_mess = filt_message.split(messages[1])
+#    print(f"message as string is -- {split_mess} --")    
+    
+#    joined_message = filt_message.join(messages[0])
+#    print(f"message as string is -- {joined_message} --")
+    
+#    filt_message = str(messages[0])
+#    print(f"filt message as string is -- {filt_message} --")
+
+    #print(f"split filt message 0 is -- {split_mess[0]} --")
+    #split_mess = str(split_mess)    
